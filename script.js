696a10673720b231332f99d29a9bb033ed882cfd
officerbarch <officerbarch@gmail.com>
2026-04-15 09:39:08 +0700

    Update script.js

--- script.js
+++ script.js
@@ -94,7 +94,7 @@ async function loadComments() {
                 <div class="comment-box">
                     <div class="comment-text">${content || ''}</div>
                     ${imgHTML}
-                    <div class="read-more" onclick="toggleExpand(this)"> lihat+ </div>
+                    <div class="read-more" onclick="toggleExpand(this)"> Lihat+ </div>
                     <div class="reaction-bar">
                         <button class="reaction-btn" onclick="addReaction(this)">❤️ <span>0</span></button>
                         <button class="reaction-btn" onclick="addReaction(this)">🫂 <span>0</span></button>
